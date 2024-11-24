50f15dbc5975cc10c66ce40c813a25d7af0b3166
helloredreyes <hello.silentlymine@gmail.com>
2024-11-25 01:17:18 +0700

    sync weather

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -83,16 +83,21 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 //weather
+//weather
 const myTown = document.querySelector('#town');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
 const myGraphic = document.querySelector('#graphic');
+const myHumidity = document.querySelector('#humidity');
+const mySunrise = document.querySelector('#sunrise');
+const mySunset = document.querySelector('#sunset');
 
 const myKey = "fd3550dbe16551fca8f4f62fc882bc6c"
 const myLat = "11.556445942513216"
 const myLong = "104.92518213140643"
 
 const myURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=metric`;
+
 
 async function apiFetch() {
     try {
@@ -109,13 +114,21 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    myTown.innerHTML = data.name
-    myDescription.innerHTML = data.weather[0].description
-    myTemperature.innerHTML = `${data.main.temp}&deg;C`
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    myGraphic.setAttribute('SRC', iconsrc)
-    myGraphic.setAttribute('alt', data.weather[0].myDescription)
+    myTown.innerHTML = data.name;
+    myDescription.innerHTML = data.weather[0].description;
+    myTemperature.innerHTML = `${data.main.temp}&deg;C`;
+    myHumidity.innerHTML = `${data.main.humidity}%`;
+
+    const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+    const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+    mySunrise.innerHTML = sunriseTime;
+    mySunset.innerHTML = sunsetTime;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    myGraphic.setAttribute('src', iconsrc);
+    myGraphic.setAttribute('alt', data.weather[0].description);
 }
+
 apiFetch();
 
 //footer
