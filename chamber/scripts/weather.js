9ea1e679abd2db37ca6ad890b94770739a71d4af
helloredreyes <hello.silentlymine@gmail.com>
2024-11-24 21:42:35 +0700

    styles

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,3 +1,41 @@
+// Toggle menu/mobile view
+const menuToggle = document.querySelector('.menu-toggle');
+const menuLinks = document.querySelector('.menu-links');
+
+menuToggle.addEventListener('click', () => {
+    menuLinks.classList.toggle('show');
+});
+
+//footer
+//current year
+//date
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+    // Display last modification date and time
+    const full = document.querySelector("#datetimemodified");
+
+    if (full) { // Check to avoid errors if the element is missing
+        const today = new Date();
+
+        const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            timeZone: "UTC"
+        };
+
+        const formattedDateTime = new Intl.DateTimeFormat("en-US", options).format(today);
+        full.innerHTML = `Last Modification: <span class="highlight">${formattedDateTime}</span>`;
+    }
+});
+
+//weather
 const myTown = document.querySelector('#town');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
